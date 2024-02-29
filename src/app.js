e061b8957cbfde4a0e48b0e5f56984eac3857fb1
gespana26 <151874827+gespana26@users.noreply.github.com>
2024-02-29 01:00:16 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//write your code here
+function nada() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let var2 = 0;
+  var2 = Math.floor(Math.random() * 5);
+  let who_r = who[var2];
+  let action_r = action[var2];
+  let what_r = what[var2];
+  let when_r = when[var2];
+  let cadena = document.getElementById("excuse");
+  //let unida =
+  cadena.innerHTML = who_r + " " + action_r + " " + what_r + " " + when_r;
+}
+window.onload = nada();
