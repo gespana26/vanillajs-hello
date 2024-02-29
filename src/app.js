58e53b57b691cff92bf559f8a2bf92812f1f27db
gespana26 <151874827+gespana26@users.noreply.github.com>
2024-02-29 01:11:06 +0000

    entrega final

--- src/app.js
+++ src/app.js
@@ -15,13 +15,15 @@ function nada() {
     "while I was praying"
   ];
   let var2 = 0;
-  var2 = Math.floor(Math.random() * 5);
+  var2 = Math.floor(Math.random() * 4);
   let who_r = who[var2];
+  var2 = Math.floor(Math.random() * 4);
   let action_r = action[var2];
+  var2 = Math.floor(Math.random() * 2);
   let what_r = what[var2];
+  var2 = Math.floor(Math.random() * 5);
   let when_r = when[var2];
   let cadena = document.getElementById("excuse");
-  //let unida =
   cadena.innerHTML = who_r + " " + action_r + " " + what_r + " " + when_r;
 }
 window.onload = nada();
